343cff4b69bce3bd3ddf71af377c52f0c5fa3ff1
Dimitri <51194271+Arcan1@users.noreply.github.com>
2023-01-19 09:22:01 +0000

    Started Campfire Saving System
    Created withinCampfireRadius bool.

--- Assets/Scripts/Campfire.cs
+++ Assets/Scripts/Campfire.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Campfire : MonoBehaviour
 {
+    public bool withinCampfireRadius;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag(GameConstants.PLAYER_TAG))
         {
             PlayerScript player = collision.gameObject.GetComponent<PlayerScript>();
             player.FullHeal();
+            withinCampfireRadius = true;
         }
     }
 }
